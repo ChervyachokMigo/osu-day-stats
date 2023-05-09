90fb8b05e39569029280106ec28bbf1b6bc4143d
tester_pivka17 <74270643+ChervyachokMigo@users.noreply.github.com>
2023-05-09 14:55:45 +0500

    delete bitwise

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var osu_username = '';
 //taiko
 //fruits
 //mania
-const game_mode = 'osu';
+const game_mode = 'taiko';
 
 //сколько процентов от максимального комбо скор считается за FC
 const combo_is_fc = 0.98; //%
